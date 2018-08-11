0e4361bae39760d16f3a6fe6cb606a6d49d84214
ctalau <cristitalau@gmail.com>
2018-08-11 15:01:23 +0300

    Hide the app-bar and make right editor read-only

--- web/plugin.js
+++ web/plugin.js
@@ -8,5 +8,15 @@
   goog.dom.appendChild(document.head, cssFile);
 
   goog.events.listenOnce(workspace, sync.api.Workspace.EventType.BEFORE_EDITOR_LOADED, function(e) {
+    // Embedded editors do not need an app-bar.
+    workspace.getViewManager().hideAppBar();
+
+    // The right editor is read-only
+    if (e.options.side === 'right') {
+      goog.events.listenOnce(workspace, sync.api.Workspace.EventType.EDITOR_LOADED, function(e) {
+        var editor = e.editor;
+        editor.setReadOnlyState({readOnly: true, code: 'diff'});
+      });
+    }
   });
 })();
